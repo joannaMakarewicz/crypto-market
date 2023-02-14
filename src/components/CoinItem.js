21ba418b486fda3b291acc104bf0203ba2cf3a0a
Joanna Makarewicz <joanna.milun@gmail.com>
2023-02-14 16:00:25 +0100

    Main structure of components & pages

---
+++ src/components/CoinItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoinItem = () => {
+  return (
+    <div>
+      CoinItem
+    </div>
+  )
+}
+
+export default CoinItem;
---
+++ src/components/Coins.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Coins.scss';
+
+const Coins = () => {
+  return (
+    <div>
+      Coins
+    </div>
+  )
+}
+
+export default Coins;

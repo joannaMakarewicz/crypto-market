cc14cf2f9d34798d463040208c91111edf8f8344
Joanna Makarewicz <joanna.milun@gmail.com>
2023-02-15 09:20:04 +0100

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import Coins from "./components/Coins";
+import Navbar from "./components/Navbar";
+import Coin from "./pages/Coin";
 import "./App.scss";
 
 function App() {
-  return <div>Hello</div>;
+  const [coins, setCoins] = useState([]);
+
+  return (
+    <div>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Coins />} />
+          <Route path="/coin" element={<Coin />}>
+            <Route path=":coinId" element={<Coin />} />
+          </Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;

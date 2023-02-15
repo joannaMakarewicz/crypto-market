9f7fc3899cf97fb06a5d8a276107b16afcb0ff26
Joanna Makarewicz <joanna.milun@gmail.com>
2023-02-15 09:34:55 +0100

    getting data by api

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ import "./App.scss";
 function App() {
   const [coins, setCoins] = useState([]);
 
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
   return (
     <div>
       <Navbar />

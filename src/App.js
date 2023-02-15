7ce2ee2511a3255b17f6e6f1c622f33518c5b515
Joanna Makarewicz <joanna.milun@gmail.com>
2023-02-15 09:42:14 +0100

    Navbar filled

--- src/App.js
+++ src/App.js
@@ -9,20 +9,24 @@ import "./App.scss";
 function App() {
   const [coins, setCoins] = useState([]);
 
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
 
   useEffect(() => {
-    axios.get(url).then((response) => {
-      setCoins(response.data);
-    }).catch((error) => {
-      console.log(error)
-    })
-  }, [])
+    axios
+      .get(url)
+      .then((response) => {
+        setCoins(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <div>
-      <Navbar />
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Coins />} />
           <Route path="/coin" element={<Coin />}>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import './Navbar.scss';
+
+import React from 'react'
+import {Link} from 'react-router-dom'
+import {FaCoins} from 'react-icons/fa'
+import './Navbar.scss'
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
+    <Link to='/'>
+        <div className='navbar'>
+            <FaCoins className='icon' />
+            <h1>Coin <span className='purple'>Search</span></h1>
+        </div>
+    </Link>
   )
 }
 
-export default Navbar;
+export default Navbar
